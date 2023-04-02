236edb6f69193d98b9a4cbf8659f6522aeb424ee
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-04-02 09:16:00 +0530

    Added : code for deleting last element of linkList

--- MyLinkList.java
+++ MyLinkList.java
@@ -36,8 +36,8 @@ public class MyLinkList {
         INode tempNode = head;
         while (tempNode.getNext() != null) {
             myNodes.append(tempNode.getKey());
-            if(!tempNode.equals(tail)) myNodes.append("->");
-              tempNode = tempNode.getNext();
+            if (!tempNode.equals(tail)) myNodes.append("->");
+            tempNode = tempNode.getNext();
         }
         myNodes.append(tempNode.getKey());
         System.out.println(myNodes);
@@ -51,34 +51,58 @@ public class MyLinkList {
             this.tail = myNewNode;
         } else {
 
-           this.tail.setNext(myNewNode) ;
-           this.tail = myNewNode;
+            this.tail.setNext(myNewNode);
+            this.tail = myNewNode;
 
         }
 
     }
-    public  void insert(INode myFirstNode, INode myNewNode) {
+
+    public void insert(INode myFirstNode, INode myNewNode) {
         if (this.head == null) {
             this.head = myNewNode;
         }
         if (this.tail == null) {
             this.tail = myNewNode;
         } else {
-                INode tempNode = myFirstNode.getNext();
-                myFirstNode.setNext(myNewNode);
-                myNewNode.setNext(tempNode);
+            INode tempNode = myFirstNode.getNext();
+            myFirstNode.setNext(myNewNode);
+            myNewNode.setNext(tempNode);
 
         }
     }
 
-    public  void pop() {
+    public void pop() {
         if (this.head == null) {
             System.out.println("List is Empty");
-        }
-        else {
-               INode tempNode = this.head.getNext();
-               this.head = tempNode;
+        } else {
+            INode tempNode = this.head.getNext();
+            this.head = tempNode;
 
         }
+    }
+
+    public void popLast() {
+        if (this.head == null) {
+            System.out.println("List is Empty");
+        return;
+        }
+        if (this.head.getNext() == null) {
+            this.head = null;
+            return;
+        }
+
+            INode secondLastNode = this.head;
+            INode lastNode = this.head.getNext();
+                while (lastNode.getNext() != null) {
+                    lastNode = lastNode.getNext();
+                    secondLastNode = secondLastNode.getNext();
+
+                }
+
+                secondLastNode.setNext(null);
+                  this.tail = secondLastNode;
+
+
     }
 }
--- MyLinkListTest.java
+++ MyLinkListTest.java
@@ -4,9 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MyLinkListTest {
-      //  UC - 5
+      //  UC - 6
     @Test
-    public void WhenGiven3NumbersDeleteFirstNumberTestCaseShouldBePass () {
+    public void WhenGiven3NumbersDeleteLastNumberTestCaseShouldBePass () {
         INode<Integer> myFirstNode = new MyNode<>(56);
         INode<Integer> mySecondNode = new MyNode<>(30);
         INode<Integer> myThirdNode = new MyNode<>(70);
@@ -14,11 +14,10 @@ public class MyLinkListTest {
         myLinkList.add(myFirstNode);
         myLinkList.append(mySecondNode);
         myLinkList.append(myThirdNode);
-
-        myLinkList.pop();
+        myLinkList.popLast();
         myLinkList.printMyNodes();
-        boolean result = myLinkList.head.equals(mySecondNode) &&
-                myLinkList.tail.equals(myThirdNode);
+        boolean result = myLinkList.head.equals(myFirstNode) &&
+                myLinkList.tail.equals(mySecondNode);
         Assert.assertTrue(result);
 
     }
